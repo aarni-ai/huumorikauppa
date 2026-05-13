83e9294d5afda175cfafd6534e740d48c6278a3a
aarni-ai <aarni@inteller.info>
2026-05-13 10:30:29 +0300

    fix: use hydrateRoot for react-snap compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById('root')!;
+const hasStaticHTML = rootElement.hasChildNodes();
+
+if (hasStaticHTML) {
+    ReactDOM.hydrateRoot(rootElement, <React.StrictMode><App /></React.StrictMode>React.StrictMode>)
+} else {
+    ReactDOM.createRoot(rootElement).render(<React.StrictMode><App /></React.StrictMode>React.StrictMode>)
+}
